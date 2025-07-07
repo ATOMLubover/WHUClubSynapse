a1669f8a48cf8cad2a1daef3935d75474658a679
kalinplus <142799819+kalinplus@users.noreply.github.com>
2025-07-07 15:12:16 +0800

    no

--- RAG/service-launcher/scripts/cpolar_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd /root/WHUCS-Qwen3
-
-/root/autodl-tmp/cpolar http 8000
-
-echo "cpolar started"
--- RAG/service-launcher/scripts/rag_server_startup.sh
+++
@@ -1,5 +0,0 @@
-cd /root/WHUCS-Qwen3
-
-uvicorn app.api.endpoints.internal_rag_service:app --host 0.0.0.0 --port 8020 --reload
-
-echo "rag server started"
